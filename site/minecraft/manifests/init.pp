af4dd8620fe3ede694b9528d8b1ba4531c8adc36
AshishDadhich4h2 <37674243+AshishDadhich4h2@users.noreply.github.com>
2018-03-27 18:11:46 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,7 +19,9 @@ $install_dir='/opt/minecraft'
     }
     file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    source => epp('minecraft/minecraft.service',{
+    install_dir => $install_dir,
+    })
     }
     service {'minecraft':
      ensure => running,
